0eb236ab58b04bf467796fd1c338f2293e37bd1f
aeagean <982381240@qq.com>
2017-08-18 02:50:51 +0800

    Add filter rules and Modify file

--- usbCan/main.cpp
+++ usbCan/main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    DWORD err = ZCOMA_Start(0, 0);
-    qDebug()<<"success!!!"<<err;
+    DWORD err = ZCOMA_Open(DEV_TYPE_USBCAN_E_P, 0, 0);
+    qDebug()<<"err code!!!"<<err;
 
     Widget* widget = new Widget;
     widget->show();
--- usbCan/utils/initDefine.h
+++ usbCan/utils/initDefine.h
@@ -2,17 +2,19 @@
 #define INITDEFINE_H
 
 #ifdef _LINUX
-#define LPVOID void*
+    #define LPVOID void*
 #else
-typedef unsigned char BOOL;
-typedef void *HANDLE;
-typedef char CHAR;
-typedef unsigned char BYTE;
-typedef unsigned short WORD;
-typedef unsigned int DWORD;
-typedef float FLOAT;
-#define LPVOID void*
+    typedef unsigned char BOOL;
+    typedef void *HANDLE;
+    typedef char CHAR;
+    typedef unsigned char BYTE;
+    typedef unsigned short WORD;
+    typedef unsigned int DWORD;
+    typedef float FLOAT;
+    #define LPVOID void*
 #endif
 
+#define DEV_TYPE_USBCAN_E_P 101
+#define CHANNELS_USBCAN_E_P 1
 #include "zcoma.h"
 #endif // INITDEFINE_H
